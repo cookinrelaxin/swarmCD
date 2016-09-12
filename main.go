3a62209dcbf504099395fe7b7bb11b9b9d085fff
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-09-12 04:17:39 -0400

    Refactor and organize code

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
     "fmt"
     "net/http"
     "encoding/json"
-    "io/ioutil"
     "os"
     "os/exec"
     "strings"
     "flag"
+    "log"
+    "errors"
+    "strconv"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    // "github.com/golang/protobuf/proto"
+    pb "./service_updater_protocol"
 )
 
 /*
@@ -26,109 +32,195 @@ import (
 */
 
 /*
-* Reference:
-* https://developer.github.com/v3/activity/events/types/#pushevent
-* https://eager.io/blog/go-and-json/
+* TODO: Rewrite the code to use log instead of fmt.
 */
 
-var dockerHubUsername string
-var dockerHubPassword string
+/*
+* TODO: Connect to the `service_updater` service to send service update messages upon successfully building a new Docker image. 
+*/
 
-var githubUsername string
-var githubPassword string
+/*
+* Reference:
+* https://developer.github.com/v3/activity/events/types/#pushevent
+*/
 
-func cloneRepository(name, url string) {
+const (
+    repositoriesDir string = "/tmp/cloned_repositories/"
+    updaterHostname string = "service_updater"
+)
 
-    defer func() {
-        if err := os.Chdir("../"); err != nil {
-            panic(err)
+var (
+    dockerHubUsername string
+    dockerHubPassword string
+
+    githubUsername string
+    githubPassword string
+)
+
+func init() {
+    /*
+    * Verify that the required command-line flags exist.
+    */
+    flag.StringVar(&dockerHubUsername, "docker-hub-username", "", "Specify your Docker Hub username")
+    flag.StringVar(&dockerHubPassword, "docker-hub-password", "", "Specify your Docker Hub password")
+    flag.StringVar(&githubUsername, "github-username", "", "Specify your Github username")
+    flag.StringVar(&githubPassword, "github-password", "", "Specify your Github password")
+    flag.Parse()
+
+    switch {
+    case dockerHubUsername == "":
+        fmt.Println("Error: -docker-hub-username required. Set -h for instructions.")
+        os.Exit(1)
+    case dockerHubPassword == "":
+        fmt.Println("Error: -docker-hub-password required. Set -h for instructions.")
+        os.Exit(1)
+    case githubUsername == "":
+        fmt.Println("Error: -github-username required. Set -h for instructions.")
+        os.Exit(1)
+    case githubPassword == "":
+        fmt.Println("Error: -github-password required. Set -h for instructions.")
+        os.Exit(1)
+    }
+}
+
+
+/*
+* Clone the specified Git repository to disk.
+*/
+func cloneRepository(repo Repository) (repositoryPath string, err error) {
+    /*
+    * Verify that the repo struct is valid.
+    */
+    switch {
+    case repo.Name == "":
+        return repositoryPath, errors.New("repo.Name not specified")
+    case repo.URL == "":
+        return repositoryPath, errors.New("repo.URL not specified")
+    }
+
+    /*
+    * Verify that `cloned_repositories` exists.
+    */
+    if _, err := os.Stat(repositoriesDir); err != nil {
+        if os.IsNotExist(err) {
+            log.Printf("Directory %s does not exist. Attempting to create.", repositoriesDir)
+            if err := os.Mkdir(repositoriesDir, 0755); err != nil {
+                return repositoryPath, err
+            }
+            log.Printf("Sucessfully created %s.", repositoriesDir)
+        } else {
+            return repositoryPath, err
         }
+    } else {
+        log.Printf("Directory %s exists. Proceeding.", repositoriesDir)
+    }
 
-        if err := os.RemoveAll(name); err != nil {
-            panic(err)
-        }
-    }()
+    repositoryPath = repositoriesDir + repo.Name
 
-    authenticated_url := "https://"+githubUsername+":"+githubPassword+"@github.com/"+githubUsername+"/"+name
-    fmt.Printf("Attempting to clone %s...\n", url)
-    gitCmd := exec.Command("git", "clone", authenticated_url)
+    /*
+    * Remove the to-be-cloned repository if it already is present.
+    */
+    if err := os.RemoveAll(repositoryPath); err != nil {
+        return repositoryPath, err
+    }
+
+    authenticated_url := "https://"+githubUsername+":"+githubPassword+"@github.com/"+githubUsername+"/"+repo.Name
+    log.Printf("Attempting to clone %s...\n", repo.URL)
+    gitCmd := exec.Command("git", "clone", authenticated_url, repositoryPath)
     gitCmd.Stdout = os.Stdout
     gitCmd.Stderr = os.Stderr
     if err := gitCmd.Run(); err != nil {
-        panic(err)
+        return repositoryPath, err
     }
-    fmt.Printf("Successfully cloned %s.\n", url)
+    log.Printf("Successfully cloned %s.\n", repo.URL)
 
-    fmt.Printf("Attempting to change directory into %s...\n", name)
-    if err := os.Chdir(name); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Successfully changed directory into %s.\n", name)
-
-    fmt.Printf("Attempting to get a new version number...\n")
-    version := getVersionNumber()
-    fmt.Printf("Successfully retrieved a new version number: %s\n", version)
-
-    fmt.Printf("Attempting to login to Docker Hub...\n")
-    loginCmd := exec.Command("docker", "login", "-u", dockerHubUsername, "-p", dockerHubPassword)
-    loginCmd.Stdout = os.Stdout
-    loginCmd.Stderr = os.Stderr
-    if err := loginCmd.Run(); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Successfully logged in to Docker Hub...\n")
-
-    fmt.Printf("Attempting to pull current Docker image from Docker Hub...\n")
-    pullCmd := exec.Command("docker", "pull", dockerHubUsername+"/"+name+":"+"latest")
-    pullCmd.Stdout = os.Stdout
-    pullCmd.Stderr = os.Stderr
-    if err := pullCmd.Run(); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Successfully pulled current Docker image.\n")
-
-    fmt.Printf("Attempting to build new Docker image from Dockerfile...\n")
-    buildCmd := exec.Command("docker", "build", "-t", dockerHubUsername+"/"+name+":"+version, ".")
-    buildCmd.Stdout = os.Stdout
-    buildCmd.Stderr = os.Stderr
-    if err := buildCmd.Run(); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Successfully built new Docker image.\n")
-
-    fmt.Printf("Attempting to push new Docker image to Docker Hub...\n")
-    pushCmd := exec.Command("docker", "push", dockerHubUsername+"/"+name+":"+version)
-    pushCmd.Stdout = os.Stdout
-    pushCmd.Stderr = os.Stderr
-    if err := pushCmd.Run(); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Successfully pushed current Docker image.\n")
-
-    fmt.Printf("Attempting to update the swarm-mode '%s' service to version %s...\n", name, version)
-    updateCmd := exec.Command("docker", "service", "update", "--image", dockerHubUsername+"/"+name+":"+version, name)
-    updateCmd.Stdout = os.Stdout
-    updateCmd.Stderr = os.Stderr
-    if err := updateCmd.Run(); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Successfully updated %s service.\n", name)
-
+    return repositoryPath, nil
 }
 
-func getVersionNumber() string {
+/*
+* Get the `version number` of a microservice from the size of its Git rev-list.
+*/
+func getVersionNumber(repositoryPath string) (versionNumber int, err error) {
+    log.Printf("Attemping to change into %s.\n", repositoryPath)
+    if err := os.Chdir(repositoryPath); err != nil {
+        return versionNumber, err
+    }
+    log.Printf("Successfully changed into %s.\n", repositoryPath)
+
+    log.Printf("Attemping to obtain version number.\n")
     gitCmd := exec.Command("git", "rev-list", "--count", "--first-parent", "HEAD")
     gitOut, err := gitCmd.Output()
     if err != nil {
-        panic(err)
+        return versionNumber, err
     }
-    version := strings.TrimSuffix(string(gitOut), "\n")
-    return version
+    log.Printf("Successfully obtained version number.\n")
+    versionNumber, err = strconv.Atoi(strings.TrimSuffix(string(gitOut), "\n"))
+    return versionNumber, err
+}
+
+/*
+* Log into Docker Hub.
+*/
+func dockerHubLogin(username, password string) error {
+    log.Printf("Attempting to login to Docker Hub...\n")
+    loginCmd := exec.Command("docker", "login", "-u", username, "-p", password)
+    loginCmd.Stdout = os.Stdout
+    loginCmd.Stderr = os.Stderr
+    if err := loginCmd.Run(); err != nil {
+        return err
+    }
+    fmt.Printf("Successfully logged in to Docker Hub...\n")
+    return nil
+}
+
+/*
+* Pull the specified Docker Image.
+*/
+func pullImage(imageName string) error {
+    log.Printf("Attempting to pull image %s from Docker Hub...\n", imageName)
+    pullCmd := exec.Command("docker", "pull", imageName)
+    pullCmd.Stdout = os.Stdout
+    pullCmd.Stderr = os.Stderr
+    if err := pullCmd.Run(); err != nil {
+        return err
+    }
+    log.Printf("Successfully pulled image.\n")
+    return nil
+}
+
+/*
+* Build an image from a Dockerfile.
+*/
+func buildImage(imageTag string) error {
+    log.Printf("Attempting to build %s from Dockerfile...\n", imageTag)
+    buildCmd := exec.Command("docker", "build", "-t", imageTag, ".")
+    buildCmd.Stdout = os.Stdout
+    buildCmd.Stderr = os.Stderr
+    if err := buildCmd.Run(); err != nil {
+        return err
+    }
+    log.Printf("Successfully built new Docker image.\n")
+    return nil
+}
+
+/*
+* Push the specified Docker Image.
+*/
+func pushImage(imageName string) error {
+    log.Printf("Attempting to push %s to Docker Hub...\n", imageName)
+    pushCmd := exec.Command("docker", "push", imageName)
+    pushCmd.Stdout = os.Stdout
+    pushCmd.Stderr = os.Stderr
+    if err := pushCmd.Run(); err != nil {
+        return err
+    }
+    fmt.Printf("Successfully pushed current Docker image.\n")
+    return nil
 }
 
 type Repository struct {
     Name string `json:"name"`
-    HTMLURL string `json:"html_url"`
+    URL string `json:"URL"`
 }
 
 type PushEvent struct {
@@ -136,50 +228,61 @@ type PushEvent struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        panic(err)
-    }
+    buf := make([]byte, r.ContentLength)
+    n, _ := r.Body.Read(buf)
     var push_event PushEvent
-    if err := json.Unmarshal(body, &push_event); err != nil {
+    if err := json.Unmarshal(buf[:n], &push_event); err != nil {
         panic(err)
     }
-    fmt.Printf("%#v\n", push_event)
+    fmt.Printf("%+v\n", push_event)
 
-    name := push_event.Repository.Name
-    url := push_event.Repository.HTMLURL
+    go func() {
+        repositoryPath, err := cloneRepository(push_event.Repository)
+        if err != nil {
+            panic(err)
+        }
+        versionNumber, err := getVersionNumber(repositoryPath)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Println(versionNumber)
 
-    cloneRepository(name, url)
+        if err := dockerHubLogin(dockerHubUsername, dockerHubPassword); err != nil {
+            panic(err)
+        }
+
+        currentImageName := dockerHubUsername + "/" + push_event.Repository.Name + ":latest"
+        if err := pullImage(currentImageName); err != nil {
+            panic(err)
+        }
+
+        newImageName := dockerHubUsername + "/" + push_event.Repository.Name + ":" + strconv.Itoa(versionNumber)
+        if err := buildImage(newImageName); err != nil {
+            panic(err)
+        }
+
+        if err := pushImage(newImageName); err != nil {
+            panic(err)
+        }
+
+        conn, err := grpc.Dial(updaterHostname, grpc.WithInsecure())
+        if err != nil {
+            log.Fatalf("Could not connect to %s: %v", updaterHostname, err)
+        }
+        defer conn.Close()
+        c := pb.NewServiceUpdaterClient(conn)
+
+        serviceName := push_event.Repository.Name
+        r, err := c.UpdateService(context.Background(), &pb.UpdateRequest{ServiceName: serviceName, ImageName: newImageName})
+        if err != nil {
+            log.Fatalf("could not update service: %v", err)
+        }
+        log.Printf("%#v", r)
+    }()
 }
 
 
 func main() {
-    flag.StringVar(&dockerHubUsername, "docker-hub-username", "", "Specify your Docker Hub username")
-    flag.StringVar(&dockerHubPassword, "docker-hub-password", "", "Specify your Docker Hub password")
-    flag.StringVar(&githubUsername, "github-username", "", "Specify your Github username")
-    flag.StringVar(&githubPassword, "github-password", "", "Specify your Github password")
-    flag.Parse()
-
-    if dockerHubUsername == "" {
-        fmt.Println("Error: -docker-hub-username required. Set -h for instructions.")
-        os.Exit(1)
-    }
-
-    if dockerHubPassword == "" {
-        fmt.Println("Error: -docker-hub-password required. Set -h for instructions.")
-        os.Exit(1)
-    }
-
-    if githubUsername == "" {
-        fmt.Println("Error: -github-username required. Set -h for instructions.")
-        os.Exit(1)
-    }
-
-    if githubPassword == "" {
-        fmt.Println("Error: -github-password required. Set -h for instructions.")
-        os.Exit(1)
-    }
-
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
